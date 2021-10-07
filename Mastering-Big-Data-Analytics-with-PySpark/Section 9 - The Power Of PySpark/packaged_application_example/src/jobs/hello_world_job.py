9bfdc86f0a6138b84f0be275cbabb2da20116f62
Trung Nghĩa Hoàng <46636991+trungnghiahoang96@users.noreply.github.com>
2021-10-07 14:28:07 +0700

    Add lecture6,7 slides

--- Mastering-Big-Data-Analytics-with-PySpark/Section 9 - The Power Of PySpark/packaged_application_example/src/jobs/hello_world_job.py
+++
@@ -1,4 +0,0 @@
-from pyspark.sql import SparkSession
-
-print("hello world from a sub-package")
-spark = SparkSession.builder.getOrCreate()
--- Mastering-Big-Data-Analytics-with-PySpark/Section 9 - The Power Of PySpark/packaged_application_example/src/main.py
+++
@@ -1,3 +0,0 @@
-from jobs.hello_world_job import spark
-
-spark.range(0, 1, 100).show()
